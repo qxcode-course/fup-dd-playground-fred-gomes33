a9f5dfe150ad7dd690e3b40fe231db832d94089d
Fred Gomes <iamfreddy33@gmail.com>
2026-06-01 18:15:40 +0000

    33° Commit

--- fup/poco/src/go/draft.go
+++ fup/poco/src/go/draft.go
@@ -1,34 +1,35 @@
 package main
+
 import "fmt"
 
 func main() {
-    var P, S, E int
-    fmt.Scan(&P, &S, &E)
+	var P, S, E int
+	if _, err := fmt.Scan(&P, &S, &E); err != nil {
+		return
+	}
 
-    posicao := 0
-    salto := S
+	posicao := 0
+	salto := S
 
-    for {
-        posSalto := posicao + salto
+	for {
+		posSalto := posicao + salto
+		if salto < 0 {
+			posSalto = posicao + salto + 10
+		}
 
-        if posSalto >= P {
-            fmt.Printf("%d saiu\n", posicao)
-            break
-        }
+		if posSalto >= P {
+			fmt.Printf("%d saiu\n", posicao)
+			break
+		}
 
-        fmt.Printf("%d %d\n", posicao, posSalto)
+		fmt.Printf("%d %d\n", posicao, posSalto)
 
-        posicao = posSalto - E
-        if posicao < 0 {
-            fmt.Printf("%d morreu\n", posicao)
-            break
-        }
+		posicao = posSalto - E
 
-        salto -= 10
-        if salto <= 0 {
-            posicao = posicao - E
-            fmt.Printf("%d morreu\n", posicao)
-            break
-        }
-    }
+		if posicao < 0 {
+			fmt.Printf("%d morreu\n", posicao)
+			break
+		}
+		salto -= 10
+	}
 }
--- sandbox/user_006/src/go/draft.go
+++ sandbox/user_006/src/go/draft.go
@@ -1,34 +1,56 @@
 package main
+
 import "fmt"
 
-// 0 -> ouros; 1 -> copas; 2 -> paus; 3 -> espadas
+// 0 "ouros", 1 "copas", 2 "paus", 3 "espadas"
 type Carta struct {
-    numero int
-    naipe int
+	numero int
+	naipe  int
 }
 
-func toString(carta Carta) string { // Carta{1,1} -> A ♥
-    var symbols = []string{"♦", "♥", "♣", "♠"}
-    naipe := symbols[carta.naipe]
-    var numeros = []string{"", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
-    return fmt.Sprintf("%s%s", numeros[carta.numero], naipe)
+type Deck struct {
+	cartas []Carta
+}
+
+func (deck *Deck) adicionarCarta(carta Carta) {
+	deck.cartas = append(deck.cartas, carta)
+}
+
+// método
+func (carta Carta) toString() string { // Carta{12, 2} -> Q♣
+	var symbols = []string{"♦", "♥", "♣", "♠"}
+	var numeros = []string{"", " A", " 2", " 3", " 4", " 5", " 6", " 7",
+		" 8", " 9", "10", " J", " Q", " K"}
+	return fmt.Sprintf("%v%v", numeros[carta.numero], symbols[carta.naipe])
+}
+
+func criarBaralho() Deck {
+	baralho := make([]Carta, 0, 52)
+	for naipe := 0; naipe <= 3; naipe++ {
+		for num := 1; num <= 13; num++ { //1
+			baralho = append(baralho, Carta{numero: num, naipe: naipe})
+		}
+	}
+
+	return Deck{baralho}
+}
+
+func (deck *Deck) puxarCarta() Carta {
+	if len(deck.cartas) == 0 {
+		panic(1)
+	}
+	carta := deck.cartas[len(deck.cartas)-1]
+	deck.cartas = deck.cartas[:len(deck.cartas)-1]
+	return carta
 }
 
 func main() {
-    for numero := 1; numero <= 13; numero++ {
-        for naipe := 0; naipe <= 3; naipe++ {
-            fmt.Printf("%v ", toString(Carta{numero, naipe}))
-        }
+	baralho := criarBaralho()
+	jogador := Deck{}
+	jogador.adicionarCarta(baralho.puxarCarta())
+	jogador.adicionarCarta(baralho.puxarCarta())
+    for _, carta := range jogador.cartas {
+        fmt.Println(carta.toString())
     }
-    fmt.Println("")
-}
 
-func criarBaralho() []Carta {
-    baralho := make([]Carta, 0, 52)
-    for naipe := 0; naipe <= 3; naipe++ {
-        for numero := 1; numero <= 13; numero++ {
-            baralho = append(baralho, Carta{numero, naipe})
-        }
-    }
-    return baralho
 }
